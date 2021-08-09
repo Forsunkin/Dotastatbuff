3991475e494463b706acd65c69362e00adf24f74
Forsunkin <guazah@gmail.com>
2021-08-09 11:01:21 +0300

    Add

--- data_base.py
+++ data_base.py
@@ -11,8 +11,7 @@ ll = {'id': 129, 'name': 'npc_dota_hero_mars', 'localized_name': 'Mars', 'primar
       '3_win': 16840, '4_pick': 39620, '4_win': 18945, '5_pick': 29048, '5_win': 13738, '6_pick': 15681, '6_win': 7555,
       '7_pick': 7716, '7_win': 3756, '8_pick': 2976, '8_win': 1394, 'null_pick': 1544841, 'null_win': 0}
 
-vv = {'id': 129, 'name': 'Mars', 'pick': 999, 'win': 555, 'winrate': 55.0, 'turbopick': 999, 'turbowin': 55,
-      'turbowinrate': 55.0, 'attr': 'str', 'img': 'http//...'}
+vv = {'id': 129, 'name': 'Mars', 'pick': 99999 }
 
 conn = sqlite3.connect('Hero_base_turbo.db')
 cursor = conn.cursor()
@@ -26,11 +25,11 @@ cursor = conn.cursor()
 #                 turbopick INT,
 #                 turbowin INT,
 #                 turbowinrate REAL,
-#                 attr TEXT,
+#                 attr TEXT
 #                 img TEXT);""")
 
 
-def winrate_count(win, pick):
+def winrate_count(pick, win):
     winrate = win / pick
     format_winrate = '{:.1f}'.format(winrate * 100)
     return format_winrate
@@ -38,26 +37,3 @@ def winrate_count(win, pick):
 
 pool_getvalues = ['id', 'localized_name', 'pick', 'win', 'winrate', 'turbo_pick', 'turbo_win', 'turbowinrate', 'attr', 'img']
 
-def winrate_raiting(obj):
-        print(sum(obj['1_win', '2_win ']))
-
-keys_win = ('1_win', '2_win', '3_win', '4_win', '5_win', '6_win', '7_win', '8_win')
-keys_pick = ('1_pick', '2_pick', '3_pick', '4_pick', '5_pick', '6_pick', '7_pick')
-
-wins_public = (sum([ll[key] for key in ll if key in keys_win]))
-pick_public = (sum([ll[key] for key in ll if key in keys_pick]))
-
-
-print(wins_public, pick_public)
-print(winrate_count(wins_public, pick_public))
-
-id = ll.get('id')
-name = ll.get('localized_name')
-pick = pick_public
-win = wins_public
-winrate = winrate_count(wins_public, pick_public)
-turbopick = ll.get('turbo_picks')
-turbowin = ll.get('turbo_wins')
-turbowinrate = winrate_count(turbowin, turbopick)
-attr = ll.get('primary_attr')
-img = ll.get('/apps/dota2/images/heroes/mars_icon.png')
