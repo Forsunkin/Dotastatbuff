4d809f32ccd150a6b42f8d39812e917e4626bd6a
Forsunkin <46975397+Forsunkin@users.noreply.github.com>
2021-08-19 10:19:10 +0300

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -4,10 +4,10 @@ bot = telebot.TeleBot('1929269998:AAFqlLEJ0jnMVcwg5FziWHyR3budKvdC1PE');
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "хуй":
+    if message.text == " ":
         bot.send_message(message.from_user.id, "")
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Хуй")
+        bot.send_message(message.from_user.id, " ")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
